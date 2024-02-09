ee4a7ae5fb6c00a3fe9cbb69ea06e16b0179a655
Raja Duraisamy <rajadura@cisco.com>
2024-02-09 16:14:59 +0530

    Helloworld, variables datatypes

---
+++ BasicsAndDatatypes.py
@@ -0,0 +1,31 @@
+#Hello world
+print("Hello world")
+
+#String Concatenation
+print("Raja"+ " " + "Duraisamy")
+
+#Escaping a String
+print("Raja said \"Hi\"")
+
+#F-Strings
+feel = "Like"
+print(f"I {feel} you")
+
+#Converting Data Types
+temp = 34
+tempFloat = float(temp)
+print(temp)
+print(tempFloat)
+
+#Converting Data Types2
+numStr = "46"
+numFl = float(numStr)
+print(float(numStr))
+
+#Checking Data Types
+pi = 3.1487
+print(type(pi))
+
+#variables
+#camelCase Variable name
+feelMyLove = "Love"
