6ac03859bbf71ca8cfbc08432bf8c8f22d296be3
bhanu prakash <prakashbhanu9398@gmail.com>
2024-02-15 12:32:05 +0530

    salary finder

---
+++ Example-programs/salary finder.py
@@ -0,0 +1,10 @@
+
+salary= 50000
+if salary <= 10000:
+    print ("house rent")
+elif salary >= 10000 and salary <= 15000:
+    print ("water and electricity bill")
+elif salary >= 15000 and salary <= 25000:
+    print ("other expenses")
+else:
+    print ("savings")    
