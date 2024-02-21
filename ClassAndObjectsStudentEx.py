9bb89347abf5130782245be023c0930dbff9eb1f
9908ganesh <157773983+9908ganesh@users.noreply.github.com>
2024-02-21 15:28:43 +0530

    examples of class and objects

--- ClassAndObjectsStudentEx.py
+++ ClassAndObjectsStudentEx.py
@@ -16,3 +16,30 @@ print(jay.getFullName())
 bhanu = Student("Bhanu", "Prakassh")
 print(bhanu.getFullName())
 
+
+print("================================================================================================================")
+class college:
+    firstName = ""
+    lastName = ""
+    group=""
+    age=""
+
+    def __init__(self, first, second,third,fourth):
+        self.firstName= first
+        self.lastName = second
+        self.group=third
+        self.age=fourth
+
+    def studentinformation(self):
+        return self.firstName +" "+ self.lastName +" "+ self.group+" "+ self.age
+
+one = college("virat","kohli","mpc","24")
+print(one.studentinformation())
+second= college("pavan","kalyan","mpc","25")
+print(second.studentinformation())
+three=college("AB","devilliears","mpc","23")
+print(three.studentinformation())
+four=college("faf","duplicis","mpc","24")
+print(four.studentinformation())
+five=college("mad","max","mpc","25")
+print(five.studentinformation())
