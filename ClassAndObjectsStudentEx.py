16b6d2551abf29371fae1802a0aa79a565f5a32c
jaya <157739427+jayakrishna15@users.noreply.github.com>
2024-02-21 14:18:44 +0530

    i am adding a another example of class and objects

--- ClassAndObjectsStudentEx.py
+++ ClassAndObjectsStudentEx.py
@@ -16,3 +16,28 @@ print(jay.getFullName())
 bhanu = Student("Bhanu", "Prakassh")
 print(bhanu.getFullName())
 
+print("______________________________________________________________________________________________________________")
+
+
+
+#Class: A class is defined using the class keyword followed by the class name. 
+#It serves as a template for creating objects.
+
+class room:
+
+    firstroom = ""
+    secondroom = ""
+
+    def __init__(self,roomno1,roomno2):
+        self.firstroom = roomno1
+        self.secondroom = roomno2
+
+    def roomname (self):
+        return self.firstroom +" "+ self.secondroom
+    
+
+room1 = room("pythonroom","pythonclasses")
+print(room1.roomname())
+
+room2 = room("javaroom","javaclasses")
+print(room2.roomname())
