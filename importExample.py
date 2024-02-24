2ca2ab5a25d997bf2234d32cd56de42c464cf210
9908ganesh <157773983+9908ganesh@users.noreply.github.com>
2024-02-24 11:50:51 +0530

    one more example of import example

--- importExample.py
+++ importExample.py
@@ -7,3 +7,13 @@ import random                   #Importing
 a = random.randint(1,100)
 
 print(a)
+
+
+import random as rd
+b=rd.randint(1,20)
+print(b)
+
+b=[]
+for m in range(10):
+    b.append(rd.randint(1,5))
+    print(b)
