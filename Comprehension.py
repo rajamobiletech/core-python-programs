34aca7feb783cbf4e28226ba899e243e8e7709d1
Raja Duraisamy <rajadura@cisco.com>
2024-02-27 09:59:18 +0530

    comprehansion and String manipulation

---
+++ Comprehension.py
@@ -0,0 +1,40 @@
+
+
+list = [0,1,2,3,4]
+list2 = []
+for i in list:
+    list2.append(i+1)
+
+print(list2)
+
+
+list3 = [i+1 for i in list2]
+print(list3)
+
+
+list4 = [4,1,2,3,4, 3,6,2]
+
+set1 = set(list4)
+print(set1)
+
+#Bug
+set2 = {k**2  for k in set1}
+print(set2)
+
+list5 = [k**2  for k in set1]
+print(list5)
+
+
+
+
+a = {'Hello':'World', 'First': 1}
+b = {}
+for k, v in a.items():
+    b.setdefault(v, k)
+print(b)
+
+c = {v: k for k , v in a.items()}
+
+print(c)
+
+
---
+++ StringManipulation.py
@@ -0,0 +1,93 @@
+
+name = "Raja"
+print(name)
+
+a = ''' Hello  World!
+This       is a 
+Multiline   String.'''
+print(a)
+
+
+a = "Python"
+print(a[0], a[2], a[4])
+print(a[-1], a[-3], a[-5])
+
+
+a = "Hello"
+# Slices the string from 0 to 3 indexes
+print(a[0:3])
+# Slices the string from 3 to -1(same as 4) indexes
+print(a[2:-1])
+
+print("================")
+
+a = "RAJA DURAISAMY"
+print(a)
+# Converts string to uppercase 
+print(a.upper())
+# Converts string to lowercase 
+print(a.lower())
+# Checks if string is uppercase 
+print(a.isupper())
+# Checks if string is lowercase 
+print(a.islower())
+
+print("================")
+b = "            "
+#Returns True if all characters in string are whitespaces
+print(b.isspace())
+c = "ajdjd%n723DSFjd73nd"
+#Returns True if given string is alphanumeric
+print(c.isalnum())
+
+a = "raja"
+# Returns True if given character is alphabet
+print(a.isalpha())
+
+#Returns True if string starts with an uppercase letter and then rest of the characters are lowercase
+print(a.istitle())
+
+
+
+
+sentence1 = "Raja Duraisamy is taking python class in the morning session"
+list = sentence1.split(' ')
+print(list)
+
+print(" * ".join(list))
+
+
+first = "first"
+second = "second"
+s = "Sunday is the {} day of the week, whereas Monday is the {} day of the week".format(first, second)
+print(s)
+
+
+
+print("================")
+
+#Returns length
+name = "raja duraisssamy"
+print(len(name))
+
+print(name.capitalize())
+
+print(name.center(30, '#'))
+
+#Find first occurence
+print(name.find("ja"))
+
+#Find start with
+print(name.find("a", 4))
+
+#Find between
+print(name.find("a", 4, 6))
+
+a = "234324"
+# Returns True if given character is number
+print(a.isdigit())
+
+
+print(name.title())
+
+print(name.title().swapcase())
